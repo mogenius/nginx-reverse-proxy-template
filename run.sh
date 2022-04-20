2df2b5958cd8317ef800ae5cd84866f134414c97
Benedikt Iltisberger <iltisberger@googlemail.com>
2022-04-20 16:52:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-chown -R 101:101 /etc/nginx
-chown -R 101:101 /usr/share/nginx/html
 cp -r -u /usr/src/nginx/config/* /etc/nginx/.
 cp -r -u /usr/src/nginx/html/* /usr/share/nginx/html/.
