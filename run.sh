463b0c952dbc94edf775e1cff36c51021b037517
Benedikt Iltisberger <iltisberger@googlemail.com>
2022-04-20 16:34:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-#cp -r -u /usr/src/nginx/config/* /etc/nginx/.
-#cp -r -u /usr/src/nginx/html/* /usr/share/nginx/html/.
-
-nginx -g 'daemon off;'
+cp -r -u /usr/src/nginx/config/* /etc/nginx/.
+cp -r -u /usr/src/nginx/html/* /usr/share/nginx/html/.
